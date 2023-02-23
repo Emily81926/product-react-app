014b82cf6b95e8842455f9bcd0faf74700bf50de
Emily81926 <emily81926@gmail.com>
2023-02-24 03:36:30 +0800

    feat: add star rating feature in ProductModal.js
    install react-rating-stars-component, add ReactStar component to ProductModal, in order to show the rating star style

--- src/components/ProductModal.js
+++ src/components/ProductModal.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import ProductsContext from "../context/products";
+import ReactStars from "react-rating-stars-component";
 
 function ProductModal() {
   const { handleClose, products, getId, getSingleProduct } =
@@ -28,12 +29,26 @@ function ProductModal() {
         <div className="card-content">
           <div className="media-content">
             <div>
-              <p className="is-size-7">{category}</p>
-              <span className="subtitle is-6 pt-2 pr-4">{rate} </span>
-              <span className="has-text-primary-dark pt-2 ">
-                {" "}
-                {count} ratings
-              </span>
+              <p className="category is-size-7 has-text-weight-bold">
+                {category}
+              </p>
+              <div className="ratings is-flex pt-2">
+                <div className="rating-star is-flex-direction-column">
+                  <ReactStars
+                    size={15}
+                    count={5}
+                    value={rate}
+                    edit={false}
+                    isHalf={true}
+                  />
+                </div>
+                <div className="rating-number is-flex-direction-column has-text-left ml-1 is-size-6">
+                  {rate}
+                </div>
+                <div className="rating-count is-flex-direction-column has-text-primary-dark ml-3 p-1 is-size-7 is-underlined">
+                  {count} ratings
+                </div>
+              </div>
               <p className="is-size-5 pb-1">${price}</p>
             </div>
             <div>
