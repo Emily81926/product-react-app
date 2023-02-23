cf04fc2cad91ba1a9e39a71b711413c0ba487073
Emily81926 <emily81926@gmail.com>
2023-02-24 04:33:43 +0800

    feat: add Hero component to App component
    add Hero banner, create Hero component and add to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "bulma/css/bulma.css";
 import ProductList from "./components/ProductList";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
+      <Hero />
       <ProductList />
     </div>
   );
