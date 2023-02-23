8cc025c2a200e4fa0a8fc8a7ad39ffb780c46898
Emily81926 <emily81926@gmail.com>
2023-02-24 03:54:01 +0800

    chore: change image size in ProductCard and delete props not used
    change image ratio from 4by3 to 4by5 in ProductCard, making products look similar to origin picture

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import ProductsContext from "../context/products";
 
-function ProductCard({ title, price, image, id, num }) {
+function ProductCard({ title, price, image, id }) {
   const { clickDetail, getClickedId } = useContext(ProductsContext);
   return (
     <div className="column is-4-desktop is-6-tablet">
       <div className="card">
         <div className="card-image px-6 p-3">
-          <figure className="image is-4by3">
+          <figure className="image is-4by5">
             <img src={image} alt={title} />
           </figure>
         </div>
@@ -15,7 +15,6 @@ function ProductCard({ title, price, image, id, num }) {
           <div className="media-content">
             <p className="title is-6">
               {title}
-              {num}
             </p>
             <p className="subtitle is-6 pt-2">${price}</p>
           </div>
